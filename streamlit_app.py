a4195d9e9712b305c1c313a5c689ea7187e60d6e
Bindu Kaimal <99102500+bindu1219@users.noreply.github.com>
2025-07-11 16:22:24 -0500

    Update streamlit_app.py
    🎯 Bring In Pandas 
    Next, we're going to add a new python library called "pandas" into our project .
    
    Remember that when we bring in a new Python library we need to add it to the requirements file, put an import statement into the app file, and reboot the app. Please do those things now. 
    
    When you bring in pandas to the Py file, call it pd in the same way we shorten streamlit to st. This is common in the Python coding world and it makes pandas easier to refer to.

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,7 @@
 import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
+import pandas as pd
 
 #Write directly to the app
 st.title(f"🥤 Customize Your Smoothie!🥤")
@@ -9,16 +10,22 @@ st.write(
   """Choose the fruits you want in your custom Smoothie!.
   """)
 
+name_on_order = st.text_input('Name on Smoothie:')
+st.write("The name on your Smoothie will be:", name_on_order)
+
+
+#session = get_active_session()
 cnx=st.connection("snowflake")
 session = cnx.session()
 
-name_on_order = st.text_input('Name on Smoothie:')
-st.write("The name on your Smoothie will be:", name_on_order)
-st.stop()
-
-#session = get_active_session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 #st.dataframe(data=my_dataframe, use_container_width=True)
+#st.stop()
+
+#Convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
+pd_df=my_dataframe.to_pandas()
+st.dataframe(pd_df)
+st.stop
 
 ingredients_list = st.multiselect(
     'Choose up to 5 ingredients:'
