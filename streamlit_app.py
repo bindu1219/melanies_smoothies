31470f2bcd70e5518bbe001fe9b9ca033610d851
Bindu Kaimal <99102500+bindu1219@users.noreply.github.com>
2025-07-12 21:25:30 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,11 +4,13 @@ from snowflake.snowpark.functions import col
 import requests
 import pandas as pd
 
+#session = get_active_session()
+cnx=st.connection("snowflake")
+session = cnx.session()
+
 #Write directly to the app
 st.title(f"🥤 Customize Your Smoothie!🥤")
-st.write(
-  """Choose the fruits you want in your custom Smoothie!.
-  """)
+st.write("""Choose the fruits you want in your custom Smoothie!.""")
 
 name_on_order = st.text_input('Name on Smoothie:')
 st.write("The name on your Smoothie will be:", name_on_order)
@@ -16,10 +18,6 @@ st.write("The name on your Smoothie will be:", name_on_order)
 my_dataframe=session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'),col('SEARCH_ON'))
 #st.dataframe(data=my_dataframe,use_container_width =True)
 #st.stop()
-
-#session = get_active_session()
-cnx=st.connection("snowflake")
-session = cnx.session()
 
 #Convert the Snowpark Dataframe to Pandas Dataframe so we can use the LOC function
 pd_df=my_dataframe.to_pandas()
